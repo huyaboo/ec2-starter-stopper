53e37d41cf892a3769750043df75263240c0c648
Huy Nguyen <73027756+huyaboo@users.noreply.github.com>
2024-07-11 16:50:48 -0700

    Add turnoff command

--- index.js
+++ index.js
@@ -3,14 +3,22 @@ const AWS = require('aws-sdk');
 AWS.config.update({region: 'us-west-2'});
 const ec2 = new AWS.EC2({apiVersion: '2016-11-15'});
 
-const instanceId = 'i-0117fa2bb812ac2ec';
+const instanceId = 'i-057f2f45f4404e3cc';
 
 
-async function doit () {
+async function turnOnCommand () {
 	if (!(await isAlive())) {
 		console.log(await turnOn());
 	} else {
 		console.log('instance is already on');
+	}
+}
+
+async function turnOffCommand () {
+	if (await isAlive()) {
+		console.log(await turnOff());
+	} else {
+		console.log('instance is already off');
 	}
 }
 
@@ -32,12 +40,32 @@ async function turnOnInstanceRequest () {
 	});
 }
 
+async function turnOffInstanceRequest () {
+	return new Promise(function (resolve, reject) {
+		ec2.stopInstances({ InstanceIds: [instanceId] }, function(err, data) {
+			if (err) {
+				reject(err)
+			} else {
+				resolve();
+			}
+		});
+	});
+}
+
 async function turnOn () {
 	await turnOnInstanceRequest();
 	while( !(await isAlive()) ) {
 		await wait500ms();
 	}
 	return 'its on';
+}
+
+async function turnOff () {
+	await turnOffInstanceRequest();
+	while(await isAlive()) {
+		await wait500ms();
+	}
+	return 'its off';
 }
 
 async function isAlive () {
@@ -60,4 +88,9 @@ async function isAlive () {
 	})	
 }
 
-doit();
+if (process.argv[2] === '-turnoff') {
+	turnOffCommand();
+}
+else {
+	turnOnCommand();
+}
